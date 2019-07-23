8bcae9ce690ff70c945ab01bf284630fcf0f4279
MeyanGitbhub <wright.brentonj@gmail.com>
2019-07-23 18:29:52 -0400

    Spring Boot Application Base.

---
+++ restarauntreservations/src/main/java/com/bj/restarauntreservations/RestarauntreservationsApplication.java
@@ -0,0 +1,13 @@
+package com.bj.restarauntreservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestarauntreservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestarauntreservationsApplication.class, args);
+	}
+
+}
