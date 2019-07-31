1173bed6c95aa425987b03e4f1334dbd23fc2c5e
MeyanGitbhub <wright.brentonj@gmail.com>
2019-07-31 17:11:30 -0400

    Make Reservation Button

--- ReservationUI/src/app/reservation/reservation.component.ts
+++ ReservationUI/src/app/reservation/reservation.component.ts
@@ -7,7 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationComponent implements OnInit {
 
-  matInput: any;
   partySizes = [2,4,6,8,10,12,14,16,18,20];
   reservationTimes = [
     "5:00 PM", "5:30 PM",
@@ -17,7 +16,7 @@ export class ReservationComponent implements OnInit {
     "9:00 PM", "9:30 PM",
     "10:00 PM", "10:30 PM",
     "11:00 PM", "11:30 PM",
-  ]
+  ];
   constructor() { }
 
   ngOnInit() {
